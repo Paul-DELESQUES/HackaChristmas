2bff742da762cb69ba295795d4421b3bfdb82e99
Sadjoaldi <alsannealdi@outlook.com>
2023-11-24 01:33:45 +0100

    cardComponent done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./styles/App.scss";
 import Welcome from "./pages/Welcome";
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
       <Welcome />
+      <Card />
     </>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import "./Card.css";
+
+function Card() {
+  return (
+    <div className="card_container">
+      <div className="scroll_container">
+        <div className="card_box">
+          <article className="card_article">
+            <img
+              src="https://img.freepik.com/vecteurs-premium/fond-arbre-noel-hiver-dans-illustration-couleur-vectorielle-plate_194708-788.jpg"
+              className="card_img"
+            />
+            <div className="card_data">
+              <span className="card_description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Officia .
+              </span>
+              <h2 className="card_title">Père noel</h2>
+              <a href="#" className="card_button">
+                Read More
+              </a>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
