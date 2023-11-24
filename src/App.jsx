5a3a3b79472af10e9e142cabf11d3b06a4f30e7d
Charles Proust <konoha33@hotmail.fr>
2023-11-24 01:20:43 +0100

    parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./styles/App.scss";
 import Actions from "./components/Actions";
+import ParallaxEffect from "./components/ParallaxEffect";
 
 function App() {
   return (
     <>
+      <ParallaxEffect />
       <Actions />
     </>
   );
